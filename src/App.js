26a526c55ceab9c7f7b6ba0a4a50a19074d72655
Zeakurbeaver <155631609+Zeakurbeaver@users.noreply.github.com>
2024-07-17 05:44:41 -0500

    Update React project with new code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import body1 from './9.svg';
 import body2 from './10.svg';
 import body3 from './11.svg';
 import body4 from './12.svg';
+import logo2 from './Logo2.png';
+import logo6 from './Compvideo.mp4';
 import './App.css';
 import gsap from 'gsap';
 import React, { useRef, useEffect, useState } from 'react';
@@ -19,22 +21,21 @@ function App() {
   const imageRefs = useRef([]);
 
   const [hasPlayed, setHasPlayed] = useState(false);
+  const [hasPlayedWrapper5, setHasPlayedWrapper5] = useState(false);
 
   useEffect(() => {
     const videoElement = videoRef.current;
     const videoElement2 = videoRef2.current;
+    const videoElement3 = videoRef3.current;
 
-    // GSAP animation with ScrollTrigger and matchMedia
     const mm = ScrollTrigger.matchMedia({
-      // Minimum width of 933px
       "(min-width: 933px)": function() {
-        // First ScrollTrigger instance for .wrapper
         const tl1 = gsap.timeline({
           scrollTrigger: {
             trigger: ".wrapper",
             start: "60% center",
-            end: "125% top",
-            markers: true,
+            end: "155% top",
+            markers: false,
             pin: true,
             pinnedContainer: ".wrapper",
             pinSpacing: true,
@@ -47,19 +48,18 @@ function App() {
           },
         });
 
-        tl1.fromTo(".wrapper", { rotation: 0 }, { rotation: 0, duration: 1 });
+        tl1.fromTo(".wrapper", { scale: 1 }, { scale: 1.2, duration: 2 });
 
         videoElement.onended = () => {
-          setHasPlayed(true); // Set hasPlayed to true once the video ends
+          setHasPlayed(true);
         };
 
-        // Second ScrollTrigger instance for .wrapper2
         const tl2 = gsap.timeline({
           scrollTrigger: {
             trigger: ".wrapper2",
             start: "70% center",
             end: "125% top",
-            markers: true,
+            markers: false,
             pinnedContainer: ".wrapper2",
             pin: true,
             pinSpacing: true,
@@ -68,21 +68,119 @@ function App() {
         });
 
         tl2.fromTo(".wrapper2", { opacity: 0 }, { opacity: 1, duration: 1 });
+
+        const tl3 = gsap.timeline({
+          scrollTrigger: {
+            trigger: ".wrapper5",
+            start: "50% center",
+            end: "180% top",
+            markers: true,
+            pin: true,
+            pinnedContainer: ".wrapper5",
+            pinSpacing: true,
+            scrub: true,
+            onEnter: () => {
+              if (!hasPlayedWrapper5) {
+                videoElement3.play();
+                
+              }
+            },
+            onLeave: () => {
+              if (!hasPlayedWrapper5) {
+                videoElement3.pause();
+              }
+            },
+          },
+        });
+
+        tl3.fromTo(".wrapper5", { opacity: 1 }, { opacity: 1, duration: 1 });
       },
     });
 
- 
-
-    // Cleanup function to kill ScrollTrigger instances
     return () => {
       mm.revert();
       ScrollTrigger.getAll().forEach(trigger => trigger.kill());
     };
-  }, []); // Ensure the useEffect runs only once and depends on hasPlayed state
+  }, [hasPlayed, hasPlayedWrapper5]);
+
+  const [dropdown1, setDropdown1] = useState(false);
+  const [dropdown2, setDropdown2] = useState(false);
+  const [dropdown3, setDropdown3] = useState(false);
+
+  const handleMouseEnter = (setDropdown) => {
+    setDropdown(true);
+  };
+
+  const handleMouseLeave = (setDropdown) => {
+    setDropdown(false);
+  };
 
   return (
     <div className="App">
-      <header className='header'><p>This is the header i think</p></header>
+      <header className='header'>
+        <a href="https://www.linkedin.com/in/stephen-santiago-09a192317/" target="_blank">
+          <img className="Logo-header" src='https://cdn.shopify.com/s/files/1/0674/2779/5200/files/AV_dan_edit.png?v=1720951752' alt="LinkedIn Profile"></img>
+        </a>
+        <div className='menu'>
+          <button
+            className='button'
+            id='button-1'
+            onClick={() => setDropdown1(!dropdown1)}
+          >
+            Dropdown menu 1
+          </button>
+          {dropdown1 && (
+            <div
+              className='dropdown'
+              onMouseEnter={() => handleMouseEnter(setDropdown1)}
+              onMouseLeave={() => handleMouseLeave(setDropdown1)}
+            >
+              <a href='#'>Option 1</a>
+              <a href='#'>Option 2</a>
+              <a href='#'>Option 3</a>
+            </div>
+          )}
+        </div>
+        <div className='menu'>
+          <button
+            className='button'
+            id='button-2'
+            onClick={() => setDropdown2(!dropdown2)}
+          >
+            Dropdown menu 2
+          </button>
+          {dropdown2 && (
+            <div className='dropdown' 
+              onMouseEnter={() => handleMouseEnter(setDropdown2)}
+              onMouseLeave={() => handleMouseLeave(setDropdown2)}
+            >
+              <a href='#'>Option 1</a>
+              <a href='#'>Option 2</a>
+              <a href='#'>Option 3</a>
+            </div>
+          )}
+        </div>
+        <div className='menu'>
+          <button
+            className='button'
+            id='button-3'
+            onClick={() => setDropdown3(!dropdown3)}
+          >
+            Dropdown menu 3
+          </button>
+          {dropdown3 && (
+            <div className='dropdown'
+              onMouseEnter={() => handleMouseEnter(setDropdown3)}
+              onMouseLeave={() => handleMouseLeave(setDropdown3)}
+            >
+              <a href='#'>Option 1</a>
+              <a href='#'>Option 2</a>
+              <a href='#'>Option 3</a>
+            </div>
+          )}
+        </div>
+      </header>
+
       <div className='wrapper'>
         <video 
           src={video} 
@@ -138,7 +236,22 @@ function App() {
         ></img>
         <h2> Web Development </h2>
       </div>
+    
+      <div className='wrapper5'>
+        <video className='comptia-video' src={logo6} ref={videoRef3}></video>
+      </div>
+      
+      <footer className='footer' >
+        <div className='footer-3rd'>
+          <p>Footer section
+          </p>
+        </div>
+</footer>
     </div>
+
+
+
+  
   );
 }
 
